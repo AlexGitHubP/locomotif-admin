3b9d498643f405fca469d779270f09b866e597a2
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2019-10-20 12:16:30 +0300

    Add files via upload

--- src/views/show_user.blade.php
+++ src/views/show_user.blade.php
@@ -1,5 +1,9 @@
+@extends('admin::header')
+@section('title', 'User ')
+@section('content')
 <p>This is user show blade</p>
 
 <p>User is {{$user->name}}</p>
 
 <a href="/admin/users">Go bec</a>
+@endsection
