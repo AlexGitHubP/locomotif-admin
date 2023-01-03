8c4beb608325a9325fed1f13c35a7961614948be
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2023-01-03 19:06:16 +0200

    Update dashboard.blade.php

--- src/views/dashboard.blade.php
+++ src/views/dashboard.blade.php
@@ -1,12 +1,16 @@
 @extends('admin::inc/header')
 @section('title', 'Admin Dashboard')
-@include('admin::inc/menu')
+
 
 @section('content')
-<div class="container">
+
+<div class="content-container">
 	<div class="cms-body">
 		<p>This is admin dashboard</p>
 	</div>
 </div>
-<p>this is another change on branch</p>
+
 @endsection()
+	
+
+
