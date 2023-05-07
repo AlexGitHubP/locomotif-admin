aafa1b01390d20a7903e0aab1f16b158798c4293
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2023-05-07 16:17:02 +0300

    Update GlobalCMSHelper.php

--- src/Helpers/GlobalCMSHelper.php
+++ src/Helpers/GlobalCMSHelper.php
@@ -1,1 +1,28 @@
+<?php
 
+if (!function_exists('mapStatus')) {
+
+    function mapStatus($status){
+        $map = array(
+            'published' => 'Publicat',
+            'pending'   => 'În așteptare',
+            'hidden'    => 'Ascuns',
+        );
+        $status = str_replace(array_keys($map), array_values($map), $status);
+        
+        return $status; 
+    }
+}
+
+
+if (!function_exists('getOrdering')) {
+    function getOrdering($table, $column='ordering'){
+        $largestNumber = DB::table($table)->max($column);
+
+        $nextNumber = $largestNumber + 1;
+
+        return $nextNumber;
+    }
+}
+
+?>
